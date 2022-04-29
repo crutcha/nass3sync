f76cb0d62bb1145847b68ee642adccb9906f6f4c
Andrew Crutchfield <andrewjcrutchfield@gmail.com>
2022-04-29 11:21:57 -0400

    additional tests

--- sync_handler_test.go
+++ sync_handler_test.go
@@ -41,7 +41,6 @@ func TestLocalFileNotInBucket(t *testing.T) {
 }
 
 func TestLocalFileIsOlder(t *testing.T) {
-	assert.True(t, true)
 	now := time.Now()
 
 	mockFileInfoResults := map[string]os.FileInfo{
@@ -52,14 +51,13 @@ func TestLocalFileIsOlder(t *testing.T) {
 	}
 	concreteWalkFunc = createMockWalkFunc(mockFileInfoResults)
 	mockBucketList := []types.Object{
-		types.Object{
+		{
 			ChecksumAlgorithm: []types.ChecksumAlgorithm{},
 			ETag:              aws.String("mock-etag"),
 			Key:               aws.String("folder2/not-real-file"),
 			LastModified:      &now,
 			Owner:             &types.Owner{},
 			Size:              1,
-			//StorageClass:      types.ObjectStorageClass{},
 		},
 	}
 	mockS3Client := NewMockClient(mockBucketList)
@@ -74,4 +72,67 @@ func TestLocalFileIsOlder(t *testing.T) {
 	assert.Nil(t, syncErr)
 	assert.Len(t, syncedObjects.TombstoneKeys, 0)
 	assert.Len(t, syncedObjects.UploadKeys, 0)
+}
+
+func TestLocalFileIsNewer(t *testing.T) {
+	oneHourAgo := time.Now().Add(-1 * time.Hour)
+	mockFileInfoResults := map[string]os.FileInfo{
+		"/folder1/folder2/not-real-file": mockFileInfo{
+			isDir:     false,
+			timestamp: time.Now(),
+		},
+	}
+	concreteWalkFunc = createMockWalkFunc(mockFileInfoResults)
+	mockBucketList := []types.Object{
+		{
+			ChecksumAlgorithm: []types.ChecksumAlgorithm{},
+			ETag:              aws.String("mock-etag"),
+			Key:               aws.String("folder2/not-real-file"),
+			LastModified:      &oneHourAgo,
+			Owner:             &types.Owner{},
+			Size:              1,
+		},
+	}
+	mockS3Client := NewMockClient(mockBucketList)
+	mockSyncConfig := SyncConfig{
+		SourceFolder:      "/folder1",
+		DestinationBucket: "not-real-bucket",
+	}
+
+	syncHandler := NewSyncHandler(mockS3Client, &sns.Client{}, mockSyncConfig, "")
+	syncedObjects, syncErr := syncHandler.Sync()
+
+	assert.Nil(t, syncErr)
+	assert.Len(t, syncedObjects.TombstoneKeys, 0)
+	assert.Len(t, syncedObjects.UploadKeys, 1)
+	assert.Contains(t, syncedObjects.UploadKeys, "/folder2/not-real-file")
+}
+
+func TestBucketFileNotOnLocalFS(t *testing.T) {
+	oneHourAgo := time.Now().Add(-1 * time.Hour)
+	mockFileInfoResults := make(map[string]os.FileInfo)
+	concreteWalkFunc = createMockWalkFunc(mockFileInfoResults)
+	mockBucketList := []types.Object{
+		{
+			ChecksumAlgorithm: []types.ChecksumAlgorithm{},
+			ETag:              aws.String("mock-etag"),
+			Key:               aws.String("folder2/not-real-file"),
+			LastModified:      &oneHourAgo,
+			Owner:             &types.Owner{},
+			Size:              1,
+		},
+	}
+	mockS3Client := NewMockClient(mockBucketList)
+	mockSyncConfig := SyncConfig{
+		SourceFolder:      "/folder1",
+		DestinationBucket: "not-real-bucket",
+	}
+
+	syncHandler := NewSyncHandler(mockS3Client, &sns.Client{}, mockSyncConfig, "")
+	syncedObjects, syncErr := syncHandler.Sync()
+
+	assert.Nil(t, syncErr)
+	assert.Len(t, syncedObjects.TombstoneKeys, 1)
+	assert.Len(t, syncedObjects.UploadKeys, 0)
+	assert.Contains(t, syncedObjects.TombstoneKeys, "/folder2/not-real-file")
 }
